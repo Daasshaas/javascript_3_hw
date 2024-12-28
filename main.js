51d1a6614dcbf37579a0350479ab9552a3b1d7ab
Daasshaas <30hanenko.dasha@gmail.com>
2024-12-28 16:47:42 +0200

    new

---
+++ main.js
@@ -0,0 +1,23 @@
+let result = 5 + 5 + '5';
+
+console.log(`${result}`);
+
+let email = "30hanenko.dasha@gmail.com";
+let symbol  = email.includes('@');
+let emailLength = email.length;
+
+console.log(`${symbol}`);
+console.log(`${emailLength}`);
+
+let word1 = "My";
+let word2 = "name";
+let word3 = "is";
+let fullName = word1 + " " + word2 + " " + word3 + " ";
+fullName = fullName + "Viktor";
+
+console.log(fullName);
+
+let userName = "Олександро";
+let payment = 300;
+
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
